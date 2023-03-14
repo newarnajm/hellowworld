73327ca8943a8ea143020f92c308e8deba8c448e
newarnajm <125204231+newarnajm@users.noreply.github.com>
2023-03-14 08:49:57 -0700

    newar

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
-
-
-
-      
+      home: HomeScreen(key: Key("value")),
     );
   }
 }
